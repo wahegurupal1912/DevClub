f06bb7d43a17f544e9c2a2f2af8fa027d326cad6
Wahegurupal Mankoo <mankoo1912@gmail.com>
2022-12-08 00:07:18 -0600

    Added first model for a Project

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional eccomerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a great project where I built out my portfolio'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project I am still working on'
+    }
+]
+
 def projects(request):
-    return render(request, 'projects/projects.html')
+    number = 9
+    context = {
+        'page': 'projects',
+        'number': number,
+        'projects': projectsList
+    }
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for project in projectsList:
+        if project['id'] == pk:
+            projectObj = project
+    return render(request, 'projects/single-project.html', {'project': projectObj})
